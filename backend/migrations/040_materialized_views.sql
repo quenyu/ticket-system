55ee4956a5afbd904b762e9c8e6f063c2eab50d0
Alice <zxa2484@gmail.com>
2025-07-02 17:54:06 +0300

    Add materialized views for departments, ticket statuses, and ticket priorities in migration file

---
+++ backend/migrations/040_materialized_views.sql
@@ -0,0 +1,3 @@
+CREATE MATERIALIZED VIEW mv_departments AS SELECT * FROM departments;
+CREATE MATERIALIZED VIEW mv_ticket_statuses AS SELECT * FROM ticket_statuses;
+CREATE MATERIALIZED VIEW mv_ticket_priorities AS SELECT * FROM ticket_priorities; 
