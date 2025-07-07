d602c36488569421d5af2d6472356e49d65b901e
Alice <zxa2484@gmail.com>
2025-07-07 21:27:56 +0300

    Add APIClient methods

--- frontend/src/network/api_client.cpp
+++ frontend/src/network/api_client.cpp
@@ -41,4 +41,106 @@ void APIClient::getDepartments(const QString &token) {
         }
         reply->deleteLater();
     });
+}
+
+void APIClient::getUsers(const QString &token) {
+    QUrl url(Config::instance().fullApiUrl() + "/users");
+    QNetworkRequest req(url);
+    req.setRawHeader("Authorization", "Bearer " + token.toUtf8());
+    QNetworkReply *reply = manager.get(req);
+    connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        if (reply->error() == QNetworkReply::NoError) {
+            emit usersReceived(reply->readAll());
+        } else {
+            emit apiError(reply->errorString());
+        }
+        reply->deleteLater();
+    });
+}
+
+void APIClient::getStatuses(const QString &token) {
+    QUrl url(Config::instance().fullApiUrl() + "/ticket_statuses");
+    QNetworkRequest req(url);
+    req.setRawHeader("Authorization", "Bearer " + token.toUtf8());
+    QNetworkReply *reply = manager.get(req);
+    connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        if (reply->error() == QNetworkReply::NoError) {
+            emit statusesReceived(reply->readAll());
+        } else {
+            emit apiError(reply->errorString());
+        }
+        reply->deleteLater();
+    });
+}
+
+void APIClient::getPriorities(const QString &token) {
+    QUrl url(Config::instance().fullApiUrl() + "/ticket_priorities");
+    QNetworkRequest req(url);
+    req.setRawHeader("Authorization", "Bearer " + token.toUtf8());
+    QNetworkReply *reply = manager.get(req);
+    connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        if (reply->error() == QNetworkReply::NoError) {
+            emit prioritiesReceived(reply->readAll());
+        } else {
+            emit apiError(reply->errorString());
+        }
+        reply->deleteLater();
+    });
+}
+
+void APIClient::createTicket(const QString &token, const QJsonObject &ticketData) {
+    QUrl url(Config::instance().fullApiUrl() + "/tickets");
+    QNetworkRequest req(url);
+    req.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    req.setRawHeader("Authorization", "Bearer " + token.toUtf8());
+    QNetworkReply *reply = manager.post(req, QJsonDocument(ticketData).toJson());
+    connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        if (reply->error() == QNetworkReply::NoError) {
+            emit ticketCreated(reply->readAll());
+        } else {
+            emit apiError(reply->errorString());
+        }
+        reply->deleteLater();
+    });
+}
+
+void APIClient::getRoles(const QString &token) {
+    QUrl url(Config::instance().fullApiUrl() + "/roles");
+    QNetworkRequest req(url);
+    req.setRawHeader("Authorization", "Bearer " + token.toUtf8());
+    QNetworkReply *reply = manager.get(req);
+    connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        if (reply->error() == QNetworkReply::NoError) {
+            emit rolesReceived(reply->readAll());
+        } else {
+            emit apiError(reply->errorString());
+        }
+        reply->deleteLater();
+    });
+}
+
+void APIClient::registerUser(const QString &username, const QString &email, const QString &password, const QString &roleId, int departmentId) {
+    QUrl url(Config::instance().fullApiUrl() + "/auth/register");
+    QNetworkRequest req(url);
+    req.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    QJsonObject obj;
+    obj["username"] = username;
+    obj["email"] = email;
+    obj["password"] = password;
+    obj["role_id"] = roleId;
+    obj["department_id"] = departmentId;
+    QNetworkReply *reply = manager.post(req, QJsonDocument(obj).toJson());
+    connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        if (reply->error() == QNetworkReply::NoError) {
+            QJsonDocument doc = QJsonDocument::fromJson(reply->readAll());
+            if (doc.isObject() && doc.object().contains("user_id")) {
+                emit registrationSuccess(doc.object()["user_id"].toString());
+            } else {
+                emit registrationFailed("Invalid response format");
+            }
+        } else {
+            emit registrationFailed(reply->errorString());
+        }
+        reply->deleteLater();
+    });
 } 
--- frontend/src/network/api_client.h
+++ frontend/src/network/api_client.h
@@ -9,10 +9,23 @@ public:
     explicit APIClient(QObject *parent = nullptr);
     void login(const QString &username, const QString &password);
     void getDepartments(const QString &token);
+    void getRoles(const QString &token);
+    void registerUser(const QString &username, const QString &email, const QString &password, const QString &roleId, int departmentId);
+    void getUsers(const QString &token);
+    void getStatuses(const QString &token);
+    void getPriorities(const QString &token);
+    void createTicket(const QString &token, const QJsonObject &ticketData);
 signals:
     void loginSuccess(const QString &token);
     void loginFailed(const QString &error);
     void departmentsReceived(const QByteArray &data);
+    void rolesReceived(const QByteArray &data);
+    void registrationSuccess(const QString &userId);
+    void registrationFailed(const QString &error);
+    void usersReceived(const QByteArray &data);
+    void statusesReceived(const QByteArray &data);
+    void prioritiesReceived(const QByteArray &data);
+    void ticketCreated(const QByteArray &data);
     void apiError(const QString &error);
 private:
     QNetworkAccessManager manager;
